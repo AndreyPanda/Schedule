c7d2e9eaf43a12e9d6e538b747fbaa39a9f44a64
https://github.com/AndreyPanda <andrey.pankin.nch@gmail.com>
2023-05-24 22:04:04 +0300

    The model "doctor" is created. Admin panel is configured to work with model "doctor".

--- schedule/main/admin.py
+++ schedule/main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from main.models import Specialization
+from main.models import Specialization, Doctor
 
 admin.site.register(Specialization)
+admin.site.register(Doctor)
+
--- schedule/main/models.py
+++ schedule/main/models.py
@@ -5,5 +5,24 @@ class Specialization(models.Model):
     visit_duration = models.IntegerField(blank=False, default=30)
     is_used = models.BooleanField(default=True)
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
+
+
+class Doctor(models.Model):
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    fathers_name = models.CharField(max_length=255)
+    specialization = models.ForeignKey(Specialization, related_name="doctors_specialization", on_delete=models.SET_NULL, null=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", null=True, blank=True)
+    education = models.TextField(max_length=2000)
+    description = models.TextField(max_length=5000, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    working_start_time = models.TimeField(blank=True)
+    working_finish_time = models.TimeField(blank=True)
+    is_active = models.BooleanField(default=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.last_name
+
